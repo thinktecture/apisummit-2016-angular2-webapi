43abb83640874fcdf31a9a948a060b2e318df126
Christian Weyer <christian.weyer@thinktecture.com>
2016-11-22 14:03:52 +0100

    Let’s keep Web API implementation

--- SimpleApi/GamesWebApi/GamesController.cs
+++ SimpleApi/GamesWebApi/GamesController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Web.Http;
 
 namespace GamesWebApi
 {
-    public class GamesController : StatusController
+    public class GamesController : ApiController
     {
         private static ConcurrentDictionary<Guid, Game> _database = 
             new ConcurrentDictionary<Guid, Game>();
@@ -16,6 +18,32 @@ namespace GamesWebApi
             _database.TryAdd(id, new Game { Id = id, Name = "Jumanji", Description = "Roaarrr!" });
             id = Guid.NewGuid();
             _database.TryAdd(id, new Game { Id = id, Name = "Wizard", Description = "Magic ;-)" });
+        }
+
+        [ActionName("list")]
+        [HttpGet]
+        public ICollection<Game> GetGames()
+        {
+            return _database.Values;
+        }
+
+        [ActionName("count")]
+        [HttpGet]
+        public int GetGamesCount()
+        {
+            return _database.Values.Count;
+        }
+
+        [ActionName("item")]
+        [HttpPost]
+        public Game AddGame(Game newGame)
+        {
+            var id = Guid.NewGuid();
+            newGame.Id = id;
+
+            _database.TryAdd(id, newGame);
+
+            return newGame;
         }
     }
 }
